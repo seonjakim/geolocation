8e174f0b228b159f6c855d13c577d76a600dcc60
seonja <seonja.kim0@gmail.com>
2022-03-01 19:26:59 +0900

    fix: fix rapid api key exposure

--- components/PlaceDetail.tsx
+++ components/PlaceDetail.tsx
@@ -1,8 +1,9 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Image, Text } from '@chakra-ui/react'
+import { Rating } from '@material-ui/lab'
 import React from 'react'
 
 type PlaceDetailProps = {
-  place: { name: string; price: string }
+  place: any
 }
 
 const PlaceDetail = ({ place }: PlaceDetailProps) => {
@@ -39,7 +40,40 @@ const PlaceDetail = ({ place }: PlaceDetailProps) => {
               {place.price}
             </Text>
           </Flex>
+          {/* Ratings */}
+          <Flex alignItems="center" width="full">
+            <Rating size="small" value={Number(place.rating)} readOnly />
+            <Text
+              fontSize="sm"
+              fontWeight="500"
+              color="gray.500"
+            >{`(${place.num_reviews})`}</Text>
+            <Text fontSize="sm" fontWeight="500" color="gray.500" ml="auto">
+              {place.price_level}
+            </Text>
+          </Flex>
+
+          {/* Ranking */}
+          <Text fontSize="sm" fontWeight="500" color="gray.400">
+            {place.ranking}
+          </Text>
+
+          {/* open status */}
+          <Text fontSize="sm" fontWeight="500" color="gray.600">
+            {place.open_now_text}
+          </Text>
         </Flex>
+        <Image
+          objectFit="cover"
+          width="120px"
+          height="120px"
+          rounded="lg"
+          src={
+            place.photo
+              ? place.photo.images.large.url
+              : 'https://explorelompoc.com/wp-content/uploads/2021/06/food_placeholder.jpg'
+          }
+        />
       </Flex>
     </Flex>
   )
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,22 +2,8 @@ import axios from 'axios'
 
 const url = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-var options = {
-  method: 'GET',
-  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-    'x-rapidapi-key': 'e249a98630msh71d6111892526dep1de288jsn7ec80b922461',
-  },
-}
-
-export const getPlacesData = async (sw, ne) => {
+type GeoType = { lat: string; lng: string }
+export const getPlacesData = async (sw: GeoType, ne: GeoType) => {
   try {
     const {
       data: { data },
